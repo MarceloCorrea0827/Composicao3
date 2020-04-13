38d304c8afe294891e956c21abb8c23767f7a132
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-13 12:27:20 -0300

    Criada a classe Product

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -6,5 +6,15 @@ namespace ExercComposição3.Entities
 {
     class Product
     {
+        public string Nome { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string nome, double price)
+        {
+            Nome = nome;
+            Price = price;
+        }
     }
 }
