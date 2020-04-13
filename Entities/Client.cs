1e659733d867f0d25c117deb4f9fac80aa4798e7
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-13 12:25:05 -0300

    Classe Client

--- Entities/Client.cs
+++ Entities/Client.cs
@@ -6,5 +6,16 @@ namespace ExercComposição3.Entities
 {
     class Client
     {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client() { }
+        public Client(string nome, string email, DateTime birthDate)
+        {
+            Nome = nome;
+            Email = email;
+            BirthDate = birthDate;
+        }
     }
 }
